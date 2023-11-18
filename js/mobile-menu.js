de1547aa205ba682231ef0c16572b8140b2a604d
Zhihare <128489441+Zhihare@users.noreply.github.com>
2023-11-18 22:02:58 +0200

    sum

---
+++ js/mobile-menu.js
@@ -0,0 +1,16 @@
+(() => {
+	const refs = {
+		openMenuBtn: document.querySelector('[data-menu-open]'),
+		closeMenuBtn: document.querySelector('[data-menu-close]'),
+		menu: document.querySelector('[data-menu]'),
+		links: document.querySelectorAll('.mobile-menu-link'),
+	};
+	refs.openMenuBtn.addEventListener('click', toggleModal);
+	refs.closeMenuBtn.addEventListener('click', toggleModal);
+	refs.links.forEach(function (link) {
+		link.addEventListener('click', toggleModal);
+	});
+	function toggleModal() {
+		refs.menu.classList.toggle('is-open');
+	}
+})();
---
+++ script.js
@@ -0,0 +1,4 @@
+import './js/mobile-menu.js';
+import './js/modal-window.js';
+import './js/berry-modal.js';
+import './js/scroll-to-top.js';
